5fab2ad0c0a08cda470f4ef93dc0940163f7f631
Karsten Zhou <1339688323@qq.com>
2023-01-28 20:08:45 +0800

    临时保存

--- 羊羊百科小助手（测试）.user.js
+++ 羊羊百科小助手（测试）.user.js
@@ -1,19 +1,20 @@
 // ==UserScript==
-// @name         羊羊百科小助手（测试）
-// @namespace    http://tampermonkey.net/
-// @version      0.1.1
-// @description  辅助收集羊羊百科需要的信息。
-// @author       宋礼
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
-// @require      https://cdn.jsdelivr.net/npm/jquery/dist/jquery.min.js
-// @match      https://www.youtube.com/playlist*
-// @match      https://item.taobao.com/item.htm*
-// @match      https://detail.tmall.com/item.htm*
-// @grant GM_download
-// @grant GM_getResourceText
-// @grant GM_addStyle
+// @name          羊羊百科小助手
+// @updateURL     https://cdn.jsdelivr.net/gh/XYY-huiji-wiki/XYY-huiji-wiki-helper@main/羊羊百科小助手.user.js
+// @downloadURL   https://cdn.jsdelivr.net/gh/XYY-huiji-wiki/XYY-huiji-wiki-helper@main/羊羊百科小助手.user.js
+// @version       0.1.1
+// @description   辅助收集羊羊百科需要的信息。
+// @author        宋礼
+// @homepage      https://github.com/XYY-huiji-wiki/XYY-huiji-wiki-helper
+// @match         https://www.youtube.com/playlist*
+// @match         https://item.taobao.com/item.htm*
+// @match         https://detail.tmall.com/item.htm*
 // @resource json file://E:\Project\XYY-huiji-wiki-helper\羊羊百科小助手.json
 // @resource html file://E:\Project\XYY-huiji-wiki-helper\羊羊百科小助手.txt
 // @resource css  file://E:\Project\XYY-huiji-wiki-helper\羊羊百科小助手.css
+// @require       https://cdn.jsdelivr.net/npm/jquery/dist/jquery.min.js
 // @require       file://E:\Project\XYY-huiji-wiki-helper\羊羊百科小助手.js
+// @grant GM_download
+// @grant GM_getResourceText
+// @grant GM_addStyle
 // ==/UserScript==
